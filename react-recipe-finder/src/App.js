031920e45129ee0478800a8832c793ccbab0988a
sevda <iremberber0@gmail.com>
2024-08-28 20:01:15 +0300

    recipe finder

---
+++ react-recipe-finder/src/App.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const Header = styled.div`
+  color: white;
+  background-color: #ea9ab2;
+  display: flex;
+  flex-direction: row;
+  padding: 20px;
+  font-size : 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #e27396;
+  
+`;
+
+function App() {
+  return (
+    <Container>
+      <Header>recipe finder</Header>
+      irem
+    </Container>
+  );
+}
+
+export default App;
